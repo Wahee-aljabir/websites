7a09a9b8d3dcef37754a12a03542d60873c26154
Wahee <waheealjabir14@gmail.com>
2024-06-08 20:56:16 +0100

    folder

--- Google/Google.js
+++ Google/Google.js
@@ -2,6 +2,6 @@ let query = document.querySelector('.query');
 let searchBtn = document.querySelector('.searchBtn');
 
 searchBtn.onclick = function(){
-    let url = 'https://www.google.com/search?q=' + query.value;
+    let url = 'https://www.google.ht/search?q=' + query.value;
     window.open(url, '_self');
 };
