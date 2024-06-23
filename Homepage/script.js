595788fae9b47c5d7b7cda79e210e60c109a2af8
Wahee <waheealjabir14@gmail.com>
2024-06-23 13:58:14 +0100

    homepage

--- Homepage/script.js
+++ Homepage/script.js
@@ -1,4 +1,10 @@
-function toggleMenu(){
+window.onload = function(){
+    window.scrollTo(0, 0);
+};
+
+function toggleMenu() {
     const MenuToggle = document.querySelector('.toggle');
+    const Sidebar = document.querySelector('.sidebar');
     MenuToggle.classList.toggle('active');
+    Sidebar.classList.toggle('active');
 };
